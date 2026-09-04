efcfdc55fb154af479cae4a09753aa8d6947a8fb
SomeRandomDeveloper <git@srd.dev>
2026-09-04 18:03:09 +0200

    Add CI (#4)
    * Fix phpcs issues
    
    * Add phan config
    
    * Add CI workflow
    
    * Add basic parser test
    
    * Add eslint/stylelint/banana CI and fix issues
    
    * Whoops
    
    * Fix parser tests
    
    * Use shared CI workflows

--- TabberHooks.php
+++ TabberHooks.php
@@ -1,4 +1,10 @@
 <?php
+
+namespace Tabber;
+
+use MediaWiki\Parser\Parser;
+use MediaWiki\Parser\PPFrame;
+
 /**
  * Tabber
  * Tabber Hooks Class
@@ -7,23 +13,17 @@
  * @author  Eric Fortin, Alexia E. Smith
  * @license GPL-3.0-only
  * @link    https://www.mediawiki.org/wiki/Extension:Tabber
-**/
-
-namespace Tabber;
-
-use MediaWiki\Parser\Parser;
-use MediaWiki\Parser\PPFrame;
-
+ */
 class TabberHooks {
 	/**
 	 * Sets up this extension's parser functions.
 	 *
-	 * @param object $parser Parser object passed as a reference.
+	 * @param Parser &$parser Parser object passed as a reference.
 	 *
-	 * @return boolean	true
+	 * @return bool true
 	 */
-	public static function onParserFirstCallInit(Parser &$parser) {
-		$parser->setHook("tabber", "Tabber\\TabberHooks::renderTabber");
+	public static function onParserFirstCallInit( Parser &$parser ) {
+		$parser->setHook( "tabber", "Tabber\\TabberHooks::renderTabber" );
 
 		return true;
 	}
@@ -31,21 +31,21 @@ class TabberHooks {
 	/**
 	 * Renders the necessary HTML for a <tabber> tag.
 	 *
-	 * @param string|null $input  The input URL between the beginning and ending tags.
-	 * @param array  $args   Array of attribute arguments on that beginning tag.
-	 * @param object $parser Mediawiki Parser Object
-	 * @param object $frame  Mediawiki PPFrame Object
+	 * @param string|null $input The input URL between the beginning and ending tags.
+	 * @param array $args Array of attribute arguments on that beginning tag.
+	 * @param Parser $parser Mediawiki Parser Object
+	 * @param PPFrame $frame Mediawiki PPFrame Object
 	 *
-	 * @return string	HTML
+	 * @return string HTML
 	 */
-	public static function renderTabber($input, array $args, Parser $parser, PPFrame $frame) {
+	public static function renderTabber( $input, array $args, Parser $parser, PPFrame $frame ) {
 		$parser->getOutput()->addModules( [ 'ext.Tabber' ] );
 
-		$key = md5($input ?? '');
-		$arr = explode("|-|", $input ?? '');
+		$key = md5( $input ?? '' );
+		$arr = explode( "|-|", $input ?? '' );
 		$htmlTabs = '';
-		foreach ($arr as $tab) {
-			$htmlTabs .= self::buildTab($tab, $parser, $frame);
+		foreach ( $arr as $tab ) {
+			$htmlTabs .= self::buildTab( $tab, $parser, $frame );
 		}
 
 		$HTML = '<div id="tabber-' . $key . '" class="tabber">' . $htmlTabs . "</div>";
@@ -56,26 +56,26 @@ class TabberHooks {
 	/**
 	 * Build individual tab.
 	 *
-	 * @param string $tab    Tab information
-	 * @param object $parser Mediawiki Parser Object
-	 * @param object $frame  Mediawiki PPFrame Object
+	 * @param string $tab Tab information
+	 * @param Parser $parser Mediawiki Parser Object
+	 * @param PPFrame $frame Mediawiki PPFrame Object
 	 *
-	 * @return string	HTML
+	 * @return string HTML
 	 */
-	private static function buildTab($tab, Parser $parser, PPFrame $frame) {
-		$tab = trim($tab);
-		if (empty($tab)) {
+	private static function buildTab( $tab, Parser $parser, PPFrame $frame ) {
+		$tab = trim( $tab );
+		if ( empty( $tab ) ) {
 			return $tab;
 		}
 
 		// Use array_pad to make sure at least 2 array values are always returned
-		list($tabName, $tabBody) = array_pad(explode('=', $tab, 2), 2, '');
+		[ $tabName, $tabBody ] = array_pad( explode( '=', $tab, 2 ), 2, '' );
 
-		$tabBody = $parser->recursiveTagParse($tabBody, $frame);
-		$tabName = $parser->getTargetLanguageConverter()->convert($tabName);
+		$tabBody = $parser->recursiveTagParse( $tabBody, $frame );
+		$tabName = $parser->getTargetLanguageConverter()->convert( $tabName );
 
 		$tab = '
-			<div class="tabbertab" data-title="' . htmlspecialchars($tabName) . '">
+			<div class="tabbertab" data-title="' . htmlspecialchars( $tabName ) . '">
 				<p>' . $tabBody . '</p>
 			</div>';
 
